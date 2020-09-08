08faf17d3fde1e077012d0d4184255d744a5e859
nalinikanta123 <nalinikantam@gmail.com>
2020-09-08 01:07:41 -0400

    Fixed the Issue two times the number

--- src/main/java/com/h2/App.java
+++ src/main/java/com/h2/App.java
@@ -4,22 +4,20 @@ package com.h2;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
 
-    public static int doubleTheNumber(int number) {
-        return number;
-    }
-    private static int add(int[] numbers) {
-    	int sum=0;
-    	for(int number:numbers) {
-    		sum=sum+number;
-    	}
-    	return sum;
-    }
+	public static int doubleTheNumber(int number) {
+		return 2 * number;
+	}
+
+	private static int add(int[] numbers) {
+		int sum = 0;
+		for (int number : numbers) {
+			sum = sum + number;
+		}
+		return sum;
+	}
 }
-
